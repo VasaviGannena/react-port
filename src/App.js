a6752aa3c5839d1226382bb986bda7125c7e5ef9
vasavi <vasavi5431@gmail.com>
2021-02-03 10:58:09 +1100

    gh-pages-react-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Components/Navbar";
+ import './App.css';
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import Navbar from "./Components/Navbar";
+  import Header from "./Components/Header";
+  import Particles from "react-particles-js";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="App">
+        <>
+      <Particles
+      className="particles-canvas"
+      params={{
+        "particles": {
+            "number": {
+                "value": 280,
+                "density": {
+                    "enable": true,
+                    "value_area": 900
+                }
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width":6,
+                "color": "#f9ab00"
+              }
+            
+            }
+            
+          }
+    }} />
+        <Navbar/>
+        <Header/>
+        </>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
