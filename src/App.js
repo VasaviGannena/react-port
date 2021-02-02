1068738597005271cebe38020f74ef3c317808b1
vasavi <vasavi5431@gmail.com>
2021-02-03 09:27:43 +1100

    gh-pages test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React-app</h1>
+      <h2>Vasavi</h2>
+    </div>
+  );
+}
+
+export default App;
